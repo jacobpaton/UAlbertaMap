0154f08c7bcb2eb70a93024a021d18d393167755
jacobpaton <jacobpaton@shaw.ca>
2019-04-28 14:48:52 -0600

    Add building and coffee shops.

--- lib/building.dart
+++ lib/building.dart
@@ -3,6 +3,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:ualberta_campus_map/feature.dart';
 
 class Building extends Feature {
+  // TODO: Hours
   String address;
 
   Building(String title, LatLng position, BuildContext context) : super(title, position, context, Icons.business, BitmapDescriptor.hueAzure);
--- lib/coffee_shop.dart
+++ lib/coffee_shop.dart
@@ -3,6 +3,7 @@ import 'package:ualberta_campus_map/feature.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CoffeeShop extends Feature {
+  // TODO: Hours, price level, menu link, phone number?
 
   CoffeeShop(String title, LatLng position, BuildContext context) : super(title, position, context, Icons.local_cafe, BitmapDescriptor.hueRose);
 }
